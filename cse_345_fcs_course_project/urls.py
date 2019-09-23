99b738d3943a3bacacdef216054491328b754965
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-09-24 00:53:43 +0530

    Added basic code for login page, now login page opens at localhost:port/login.

--- cse_345_fcs_course_project/urls.py
+++ cse_345_fcs_course_project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('site/', include('main_app.urls')),
+    path('', include('main_app.urls')),
     path('admin/', admin.site.urls),
 ]
