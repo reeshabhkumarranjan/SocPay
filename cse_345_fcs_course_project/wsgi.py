531ee05be51a902812e16e5278d9fa50b2e4e6f8
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-09-23 19:22:22 +0530

    Initial commit

---
+++ cse_345_fcs_course_project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cse_345_fcs_course_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cse_345_fcs_course_project.settings')
+
+application = get_wsgi_application()
