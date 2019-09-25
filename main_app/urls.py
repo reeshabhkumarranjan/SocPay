cc879ca7bf632f054c3b4d8db5ab0680cb92b95a
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-09-26 01:29:53 +0530

    Added dummy pages for login and register post requests.

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ app_name = 'polls'
 urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
-    path('index', views.index, name='index')
+    path('index', views.index, name='index'),
+    path('register_form', views.register_form, name='register_form'),
+    path('login_form', views.login_form, name='login_form'),
 ]
