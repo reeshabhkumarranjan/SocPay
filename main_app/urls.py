3b3cfb85940e4264e9878c23fdc820bb6bdca43c
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-09-25 20:16:09 +0530

    Added index page. Started to implement inheritance in django templates.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('login', views.login, name='login')
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('index', views.index, name='index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def index(request):
+    return render(request, 'index.html')
