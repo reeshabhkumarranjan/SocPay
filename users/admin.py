1172d448358d37bd24d1fef0dbaa4a55ba2b3bbb
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-09-26 21:26:00 +0530

    Work related to models done.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+from users.forms import BaseUserCreationForm, BaseUserChangeForm
+from users.models import BaseUser
+
+
+class BaseUserAdmin(UserAdmin):
+    add_form = BaseUserCreationForm
+    form = BaseUserChangeForm
+    model = BaseUser
+    list_display = ['email', 'username']
+
+
+admin.site.register(BaseUser, BaseUserAdmin)
