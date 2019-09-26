dbb12e175498f1bc8feda5ce96e7344afb822bfa
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-09-26 16:28:20 +0530

    Removed redundant code.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 app_name = 'main_app'
 urlpatterns = [
-    path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('index', views.index, name='index'),
-    path('register_form', views.register_form, name='register_form'),
-    path('login_form', views.login_form, name='login_form'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,40 +1,8 @@
 from django.shortcuts import render
 
-
-# Create your views here.
-
-
-def login(request):
-    """This is only to display login page."""
-    return render(request, 'login.html')
-
-
-def login_form(request):
-    """This handles a login request."""
-    # TODO complete this method
-    username = request.POST["username"]
-    password = request.POST["password"]
-    context = {'test_string' : username + " " + password}
-
-    return render(request, 'dummy.html', context=context)
-
-
 def register(request):
     """This just displays the register page."""
     return render(request, 'register.html')
-
-
-def register_form(request):
-    """This handles the register request."""
-    # TODO complete this method
-    name = request.POST["name"]
-    email = request.POST["email"]
-    dob = request.POST["dob"]
-    username = request.POST["username"]
-
-    context = {'test_string' : name + " " + email + " " + dob + " " + username}
-
-    return render(request, 'dummy.html', context = context)
 
 
 def index(request):
