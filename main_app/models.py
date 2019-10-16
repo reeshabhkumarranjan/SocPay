f19dcb276a22873e9bb46ca182ad16d347ad58f8
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-10-16 21:46:10 +0530

    Added timeline. Timeline's posts section is not displaying properly though.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    # author_name = usermodel.BaseUser()
+    author_name = models.CharField(max_length=30)
+    post_date = models.DateTimeField(auto_now_add=True, blank=True)
+    # recipient_name = usermodel.BaseUser()
+    recipient_name = models.CharField(max_length=30)
+    post_text = models.CharField(max_length=1000)
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from main_app.models import Post
+
+
 def register(request):
     """This just displays the register page."""
     return render(request, 'signup.html')
@@ -12,3 +15,8 @@ def login(request):
 
 def index(request):
     return render(request, 'index.html')
+
+def timeline(request):
+    all_posts = Post.objects.all()
+    context = {'forloop' : range(100), 'all_posts' : all_posts}
+    return render(request, 'timeline.html', context=context)
