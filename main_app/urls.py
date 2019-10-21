60d3f361bd9d020c42573fb6224dbc129be7407f
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-10-21 20:19:33 +0530

    Now we can post on our friend's timeline.

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('timeline', views.timeline, name='timeline'),
     path('friends', views.friends, name='friends'),
     path('add_post', views.add_post, name='add_post'),
-    path('friend_timeline/<slug:friend_username>', views.friend_timeline, name='friend_timeline')
+    path('friend_timeline/<slug:friend_username>', views.friend_timeline, name='friend_timeline'),
+    path('add_post_friend/<slug:friend_username>', views.add_post_friend, name='add_post_friend')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -18,7 +18,7 @@ def index(request):
     return render(request, 'index.html')
 
 def timeline(request):
-    all_posts = Post.objects.filter(author_name=request.user.username, recipient_name=request.user.username).order_by('-post_date')
+    all_posts = Post.objects.filter(recipient_name=request.user.username).order_by('-post_date')
     context = {'forloop' : range(100), 'all_posts' : all_posts}
     return render(request, 'timeline.html', context=context)
 
@@ -37,6 +37,17 @@ def add_post(request):
     Post.objects.create(author_name=author_name, recipient_name=recipient_name, post_text=post_text)
     return redirect('main_app:timeline')
 
+def add_post_friend(request, friend_username):
+    # TODO add checks
+    author_name = request.user.username
+    recipient_name = friend_username
+    post_text = request.POST.get('post_text', "N/A")
+
+    Post.objects.create(author_name=author_name, recipient_name=recipient_name, post_text=post_text)
+    return redirect('main_app:friend_timeline', friend_username=friend_username)
+    # return redirect('https://google.com')
+
 def friend_timeline(request, friend_username):
-    context = {'friend_username' : friend_username}
+    all_posts = Post.objects.filter(recipient_name=friend_username).order_by('-post_date')
+    context = {'friend_username' : friend_username, 'all_posts' : all_posts}
     return render(request, 'friend_timeline.html', context=context)
