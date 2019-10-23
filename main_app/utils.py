faf87a9247f585f4ac41b50f483db6c24826e057
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-10-23 21:25:49 +0530

    Started with friend request backend.

---
+++ main_app/utils.py
@@ -0,0 +1,44 @@
+from users.models import Friend, CustomUser
+
+
+def get_friends(user):
+    # print("nikal")
+    barr = Friend.objects.all();
+    # print(barr)
+    # print(user)
+    arr = Friend.objects.filter(creator=user, confirmed=True).values_list("follower")
+    arr1 = Friend.objects.filter(follower=user, confirmed=True).values_list("creator")
+    friend_arr = []
+    # print(arr)
+    # print(arr1)
+    for i in arr:
+        # print(type(i[0]))
+        friend_arr.append(CustomUser.objects.get(pk=i[0]))
+    for i in arr1:
+        friend_arr.append(CustomUser.objects.get(pk=i[0]))
+    return friend_arr
+
+
+def get_received_requests(user):
+    arr = Friend.objects.filter(follower=user, confirmed=False).values_list("creator")
+    friend_arr = []
+    for i in arr:
+        friend_arr.append(CustomUser.objects.get(id=i[0]))
+    return friend_arr
+
+
+def get_sent_requests(user):
+    arr = Friend.objects.filter(creator=user, confirmed=False).values_list("follower")
+    friend_arr = []
+    for i in arr:
+        friend_arr.append(CustomUser.objects.get(id=i[0]))
+    return friend_arr
+
+
+def get_not_friends(friends, requests_sent, requests_received):
+    arr = CustomUser.objects.all()
+    not_friends = []
+    for i in arr:
+        if i not in friends and i not in requests_received and i not in requests_sent:
+            not_friends.append(i)
+    return not_friends
--- users/views.py
+++ users/views.py
@@ -1,14 +1,92 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
+from django.views.generic import TemplateView
 
+from main_app.utils import get_friends, get_sent_requests, get_received_requests, get_not_friends
 from users.forms import CustomUserCreationForm
+from users.models import Friend
 
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class Friendship(TemplateView):
+    template_name = 'friends.html'
+
+    # def get(self, request, *args, **kwargs):
+    #     if request.method == "Post":
+    #         if request.POST.get("no_friends"):
+    #             friend_name = request.POST.get('friends', 'default')
+    #             print(friend_name)
+    #     return HttpResponseRedirect(reverse('friends', args=[request.user.username]))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['friends_list'] = get_friends(self.request.user)
+        context['requests_sent'] = get_sent_requests(self.request.user)
+        context['requests_received'] = get_received_requests(self.request.user)
+        context['not_friends'] = get_not_friends(context['friends_list'], context['requests_sent'], context['requests_received'])
+        #print(context['friends_list'])
+        # for friend in context['friends_list']:
+            #print(friend)
+        # print()
+        # for friend in context['requests_sent']:
+        #     print(friend)
+        # # print()
+        # for friend in context['requests_received']:
+        #     print(friend)
+        # # print()
+        # for friend in context['not_friends']:
+        #     print(friend)
+        return context
+
+
+def add_friend(request):
+    # print(request.user.id)
+
+    friend_id = request.POST.get('friend', 'default')
+    Friend.objects.create(creator_id=request.user.id, follower_id=friend_id, confirmed=False)
+    return HttpResponseRedirect(reverse('friends', args=[request.user.username]))
+
+
+def decline(request):
+    friend_id = request.POST.get('friend', 'default')
+    # print(friend)
+    Friend.objects.filter(creator_id=friend_id, follower_id=request.user.id, confirmed=False).delete()
+    return HttpResponseRedirect(reverse('friends', args=[request.user.username]))
+
+
+def accept(request):
+    # print("hiiiiiiii")
+    friend_id = request.POST.get('friend', 'default')
+    row = Friend.objects.get(creator_id=friend_id, follower_id=request.user.id, confirmed=False)
+    row.confirmed = True
+    row.save()
+    # print("hi")
+    # print(Friend.objects.get(creator_id=friend_id,follower_id=request.user.id).confirmed)
+    return HttpResponseRedirect(reverse('friends', args=[request.user.username]))
+
+
+def cancel(request):
+    friend_id = request.POST.get('friend', 'default')
+    Friend.objects.filter(creator_id=request.user.id, follower_id=friend_id).delete()
+    return HttpResponseRedirect(reverse('friends', args=[request.user.username]))
+
+
+def remove_friend(request):
+    friend_id = request.POST.get('friend', 'default')
+    row = Friend.objects.get(creator_id=request.user.id, follower_id=friend_id)
+    # print("ro")
+    # print(row)
+    if not row:
+        row = Friend.objects.get(creator_id=friend_id, follower_id=request.user.id)
+    # print(row)
+    row.delete()
+    return HttpResponseRedirect(reverse('friends', args=[request.user.username]))
 
