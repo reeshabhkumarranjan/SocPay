fce8642450036a29279244236948918ffd11c010
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-10-26 03:52:17 +0530

    Fixed the friends-view which got outdated during integrating the wallet.

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ import pyotp as pyotp
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from datetime import datetime
+
+from main_app import utils
 from main_app.models import Post, Transaction
 from users.models import Friend, CustomUser
 from .forms import transaction_form
@@ -22,23 +24,21 @@ def login(request):
 def index(request):
     return render(request, 'index.html')
 
-
 def timeline(request):
     all_posts = Post.objects.filter(recipient_name=request.user.username).order_by('-post_date')
-    context = {'forloop': range(100), 'all_posts': all_posts}
+    context = {'forloop' : range(100), 'all_posts' : all_posts}
     return render(request, 'timeline.html', context=context)
-
 
 def friends(request):
     if request.method == 'POST':
-        pass  # TODO complete this
+        pass # TODO complete this
     # all_friends = Friend.objects.all(creator__id=)
-    all_friends = (Friend.objects.filter(creator=request.user) | Friend.objects.filter(
-        follower=request.user)) & Friend.objects.filter(confirmed=True)
-    all_strangers = CustomUser.objects.all()
-    context = {'all_friends': all_friends, 'all_strangers': all_strangers}
+    all_friends = utils.get_friends(request.user)
+    all_strangers = utils.get_not_friends(request.user)
+    all_requests_sent = utils.get_sent_requests(request.user)
+    all_requests_received = utils.get_received_requests(request.user)
+    context = {'all_friends' : all_friends, 'all_strangers' : all_strangers, 'all_requests_sent' : all_requests_sent, 'all_requests_received' : all_requests_received}
     return render(request, 'friends.html', context=context)
-
 
 def add_post(request):
     # TODO add checks
@@ -48,7 +48,6 @@ def add_post(request):
 
     Post.objects.create(author_name=author_name, recipient_name=recipient_name, post_text=post_text)
     return redirect('main_app:timeline')
-
 
 def add_post_friend(request, friend_username):
     # TODO add checks
@@ -60,10 +59,9 @@ def add_post_friend(request, friend_username):
     return redirect('main_app:friend_timeline', friend_username=friend_username)
     # return redirect('https://google.com')
 
-
 def friend_timeline(request, friend_username):
     all_posts = Post.objects.filter(recipient_name=friend_username).order_by('-post_date')
-    context = {'friend_username': friend_username, 'all_posts': all_posts}
+    context = {'friend_username' : friend_username, 'all_posts' : all_posts}
     return render(request, 'friend_timeline.html', context=context)
 
 
