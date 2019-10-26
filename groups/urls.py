b70c9aa8157ee6aedbc01ab3c8a382e659857f93
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-10-26 18:13:36 +0530

    Added post-adding feature in group timeline.

--- groups/urls.py
+++ groups/urls.py
@@ -6,11 +6,12 @@ app_name = 'groups'
 urlpatterns = [
     path('group/', views.ShowGroups.as_view(), name='group'), # sare grps display hote hai, leave, add, pending.
     path('admin/', views.MyGroups.as_view(), name='group_admin'), # my owned group. btton of groups.
-    path('view/', views.groupsView, name='group_view'), #
+    path('view/<int:group_id>', views.groupsView, name='group_view'), #
     path('add/', views.addgroup, name='add_group'), #
     path('join/', views.AddJoinRequest, name='join_group'), #
     path('cancel/', views.cancelJoinRequest, name='cancel_request'),
     path('remove/', views.removeFromGroup, name='remove'),
     path('accept/', views.acceptJoinRequest, name='accept'),
     path('decline/', views.rejectJoinRequest, name='decline'),
+    path('add_group_post/', views.add_group_post, name='add_group_post')
 ]
